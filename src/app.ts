791e70f2bf949e03169cb9ce8b51881fe95b6462
horiiiiii0329 <85778704+horiiiiii0329@users.noreply.github.com>
2021-11-03 22:24:44 +0900

    print and clear field

--- src/app.ts
+++ src/app.ts
@@ -48,10 +48,39 @@ class ProjectInput {
     this.attach();
   }
 
+  private clearinput() {
+    this.titleInputElement.value = "";
+    this.descriptionInputElement.value = "";
+    this.peopleInputElement.value = "";
+  }
+
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredPeople = this.peopleInputElement.value;
+
+    if (
+      enteredTitle.trim().length === 0 ||
+      enteredDescription.trim().length === 0 ||
+      enteredPeople.trim().length === 0
+    ) {
+      alert("Invalid Input");
+      return;
+    } else {
+      return [enteredTitle, enteredDescription, +enteredPeople];
+    }
+  }
+
   @autobind
   private submitHandler(e: Event) {
     e.preventDefault();
     console.log(this.titleInputElement.value);
+    const userInput = this.gatherUserInput();
+    if (Array.isArray(userInput)) {
+      const [title, desc, people] = userInput;
+      console.log(title, desc, people);
+      this.clearinput();
+    }
   }
 
   private configure() {
